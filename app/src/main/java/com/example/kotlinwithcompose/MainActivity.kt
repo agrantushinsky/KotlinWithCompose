9211af8741cacf0b0be907a1fde4c4bc41836a67
2183276 <a@amazon.com>
2023-08-30 14:42:59 -0400

    i made an error

--- app/src/main/java/com/example/kotlinwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwithcompose/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.kotlinwithcompose.ui.MyScreen
 import com.example.kotlinwithcompose.ui.theme.KotlinWithComposeTheme
 
 //....
+laskdfhiophiosdfghiodfghio
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

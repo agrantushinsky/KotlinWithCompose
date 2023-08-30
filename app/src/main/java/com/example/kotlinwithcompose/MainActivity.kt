c151233992fbc8ed6f3c15e77aad81cf14c43a8b
2183276 <a@amazon.com>
2023-08-30 14:46:53 -0400

    i made an error again

--- app/src/main/java/com/example/kotlinwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwithcompose/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
             KotlinWithComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.ze(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     MyScreen()
